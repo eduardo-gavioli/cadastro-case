84173d0a1c4f223ffd0d4f7574f5cfa800ccb593
Eduardo Gavioli <edumapfre@gmail.com>
2025-05-16 01:34:22 -0300

    feat: refactor Cadastro entity and DTO structure

---
+++ src/main/java/com/cadatro/demo/domain/entity/CadastroModel.java
@@ -0,0 +1,64 @@
+package com.cadatro.demo.domain.entity;
+
+import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Table(name = "cadastro")
+@Entity
+public class Cadastro extends RepresentationModel<Cadastro> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String pais;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
